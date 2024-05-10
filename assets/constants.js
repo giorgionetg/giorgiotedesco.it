c940aa83a4e42a0165cb78ecf21dcf6f37d95d85
giorgionetg <giorgionetg@gmail.com>
2024-05-10 14:53:33 +0200

    reset over

--- assets/constants.js
+++
@@ -1,7 +0,0 @@
-import { AppConfig } from "@stacks/auth";
-
-export const appConfig = new AppConfig(["store_write", "publish_data"]);
-
-export const POSTS_FILENAME = "posts.json";
-
-export const EXPLORER_URL = "https://explorer.blockstack.org";
--- myContentLoader/mdLoader.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default async function mdLoader(category) {
-  let maindir = path.resolve(process.cwd(), "contents/" + category);
-  return await fs.readdirSync(maindir);
-}
