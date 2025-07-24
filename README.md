cb0ec8b26356a188c9ec6a5e15a421f2fcda44ff
giorgionetg <giorgionetg@gmail.com>
2025-07-24 07:46:25 +0200

    add alls

---
+++ README.md
@@ -0,0 +1,1 @@
+# GiorgioTedesco.it
---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+import createMDX from '@next/mdx';
+
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
