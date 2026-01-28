a306e5db8ce338137e9059e44a7817d614cc9d52
giorgionetg <gt@giorgiotedesco.it>
2026-01-28 07:51:53 +0100

    Fix: Correct tabIndex type in landing/navbar

--- src/app/components/landing/navbar.tsx
+++ src/app/components/landing/navbar.tsx
@@ -1,54 +1,54 @@
 export default async function Navbar() {
-    return(
+    return (
         <div className="w-full flex justify-center center bg-base-100 ">
             <div className="navbar max-w-[1220px] shadow-sm">
                 <div className="navbar-start">
                     <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/digital-garden'>About Me</a></li>
-                        <li>
-                        <a>Parent</a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/digital-garden'>About Me</a></li>
+                            <li>
+                                <a>Parent</a>
+                                <ul className="p-2">
+                                    <li><a>Submenu 1</a></li>
+                                    <li><a>Submenu 2</a></li>
+                                </ul>
+                            </li>
+                            <li><a>Item 3</a></li>
                         </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Giorgio Tedesco</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/digital-garden/web-development'>About Me</a></li>
-                    <li>
-                        <details>
-                        <summary>My Digital Garden</summary>
-                        <ul className="p-2 bg-base-100 w-40 z-1">
-                            <li><a href='/digital-garden'>Digital's Giorgio Garden</a></li>
-                            <li><a>Frontend</a></li>
-                            <li><a>Backend</a></li>
-                            <li><a>Fullstack</a></li>
-                        </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary>Solutions</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Website Development</a></li>
-                                <li><a>PMI and Freelance Tools</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Write Me</a></li>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/digital-garden/web-development'>About Me</a></li>
+                        <li>
+                            <details>
+                                <summary>My Digital Garden</summary>
+                                <ul className="p-2 bg-base-100 w-40 z-1">
+                                    <li><a href='/digital-garden'>Digital's Giorgio Garden</a></li>
+                                    <li><a>Frontend</a></li>
+                                    <li><a>Backend</a></li>
+                                    <li><a>Fullstack</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li>
+                            <details>
+                                <summary>Solutions</summary>
+                                <ul className="p-2 bg-base-100 w-40 z-1">
+                                    <li><a>Website Development</a></li>
+                                    <li><a>PMI and Freelance Tools</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><a>Write Me</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
