229092b58b57c45ec4cd78d7dfebc733821d40a3
giorgionetg <gt@giorgiotedesco.it>
2026-01-28 07:45:52 +0100

    Fix: Remove invalid boxGeometry prop causing build failure

--- src/app/components/canvas/CanvasComponent.tsx
+++ src/app/components/canvas/CanvasComponent.tsx
@@ -4,19 +4,20 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 
-export default function CanvasComponent () {
+export default function CanvasComponent() {
     return (
         <main >
-            <Canvas style={{ width: "100vw", height: "100vh", position: "fixed", 
+            <Canvas style={{
+                width: "100vw", height: "100vh", position: "fixed",
                 inset: 0,
                 zIndex: 0,
                 pointerEvents: "none",
-                }} camera={{ position: [3, 3, 3] }}>
+            }} camera={{ position: [3, 3, 3] }}>
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[5, 5, 5]} />
                 <mesh>
                     <boxGeometry />
-                    <boxGeometry position={{ position: [1, 0, 0]}} />
+
                     <meshStandardMaterial color="orange" />
                 </mesh>
                 <OrbitControls />
